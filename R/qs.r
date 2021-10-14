f871f5ac7b1d7a6add799f023b23d10ce899d5d6
Charlie <zsong6@ncsu.edu>
2021-10-14 02:39:39 -0400

    sim

--- R/qs.r
+++ R/qs.r
@@ -23,11 +23,10 @@
 #'
 
 qs_scam <- function(x, y, maxit = 10, ntaus = 10,
-                arg_bs = c("tesmi1", "ps"),
-                opt = "bfgs", dims = c(5, 15), ords = NA,
-                analysis=F, trace=F, sp = NULL, tol = 5e-2,
-                bnd = 1e-10, ...) {
-
+                    arg_bs = c("tesmi1", "ps"),
+                    opt = "bfgs", dims = c(5, 15), ords = NA,
+                    analysis = F, trace = F, sp = NULL, tol = 5e-2,
+                    bnd = 1e-5, ...) {
     if (is.vector(x)) {
         s_k <- 1
     } else if (is.matrix(x)) {
@@ -36,16 +35,16 @@ qs_scam <- function(x, y, maxit = 10, ntaus = 10,
         stop("input x type not support")
     }
 
-    #initialization
+    # initialization
     fit <- cq.reg(x, y, arg_bs, opt, dims, ords, sp = sp, ...)
     v_coef <- coef(fit)
-    #fit <- f_descent(y, x, dims - ords, ords, maxit = 0)
-    #v_coef <- fit$beta
+    # fit <- f_descent(y, x, dims - ords, ords, maxit = 0)
+    # v_coef <- fit$beta
 
-    #repeat Y ntaus times
+    # repeat Y ntaus times
     ystar <- rep(y, ntaus)
 
-    #data
+    # data
     v_taus <- seq(bnd, 1 - bnd, length.out = ntaus)
     v_wog <- v_taus[cut(seq_along(ystar), breaks = ntaus, labels = FALSE)]
     xnam <- c("tau", paste0("x", seq_len(s_k)))
@@ -53,41 +52,46 @@ qs_scam <- function(x, y, maxit = 10, ntaus = 10,
     dat <- data.frame(ystar, newx)
     colnames(dat) <- c("y", xnam)
 
-    #get info
+    # get info
     v_pred <- predict(fit, dat)
-    #v_pred <- c(cq_prediction(v_taus, fit))
+    # v_pred <- c(cq_prediction(v_taus, fit))
     v_resid <- ystar - v_pred
 
-    #Schlossmacher's method
+    # Schlossmacher's method
     for (iter in seq(maxit)) {
-        #weights
+        # weights
         v_idx <- ystar <= v_pred
         v_weight <- v_wog
         v_weight[v_idx] <- 1 - v_weight[v_idx]
-        v_weight <- v_weight / sqrt(v_resid^2 + 1e-8*max(abs(v_resid))^2)
+        v_weight <- v_weight / sqrt(v_resid^2 + 1e-8 * max(abs(v_resid))^2)
         v_weight <- v_weight / mean(v_weight)
-        #fit the model
+        # fit the model
         fit <- scam(y ~ s(tau, x1, k = dims, m = ords, bs = arg_bs),
             data = dat, optimizer = opt, sp = c(sp), weights = c(v_weight),
-            start = NULL, ...)
+            start = v_coef, ...
+        )
 
-        #get info
+        # get info
         v_pred <- fit$fitted.values
         v_resid <- fit$residual
         s_diff <- norm_2(v_coef - c(coef(fit))) / norm_2(v_coef)
         v_coef <- c(coef(fit))
-        #plot the trace
+        # plot the trace
         if (trace) {
-            fit$x <- x; fit$y <- y; fit$xnam <- xnam
+            fit$x <- x
+            fit$y <- y
+            fit$xnam <- xnam
             sub <- paste("Iter:", iter, "diff:", round(s_diff, 3))
             main <- paste("Smoothing:", fit$sp, "dim:", dims)
             cq_regplot(fit, sub = sub, main = main)
         }
-        #terminate
+        # terminate
         if (s_diff <= tol) break
     }
 
-    fit$x <- x; fit$y <- y; fit$xnam <- xnam
+    fit$x <- x
+    fit$y <- y
+    fit$xnam <- xnam
     if (analysis) {
         dev.new()
         par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))
@@ -95,7 +99,8 @@ qs_scam <- function(x, y, maxit = 10, ntaus = 10,
         plot(fit, pers = TRUE, theta = 30, phi = 40)
         plot(ystar, fit$fitted.values,
             xlab = "Real Data",
-            ylab = "Fitted data", pch = ".", cex = 3)
+            ylab = "Fitted data", pch = ".", cex = 3
+        )
         cq_regplot(fit, sub = sub, main = main)
     }
     fit$ntaus <- ntaus
@@ -132,9 +137,8 @@ qs_scam <- function(x, y, maxit = 10, ntaus = 10,
 #'
 
 qs_gam <- function(x, y, maxit = 10, ntaus = 10,
-                dims = c(10, 10), ords = NA,
-                analysis=F, trace=F, sp = NULL, tol = 1e-3, ...) {
-
+                   dims = c(10, 10), ords = NA,
+                   analysis = F, trace = F, sp = NULL, tol = 1e-3, ...) {
     if (is.vector(x)) {
         s_k <- 1
     } else if (is.matrix(x)) {
@@ -142,10 +146,10 @@ qs_gam <- function(x, y, maxit = 10, ntaus = 10,
     } else {
         stop("input x type not support")
     }
-    #repeat Y ntaus times
+    # repeat Y ntaus times
     ystar <- rep(y, ntaus)
 
-    #data
+    # data
     v_taus <- seq(0, 1, length.out = ntaus)
     v_wog <- v_taus[cut(seq_along(ystar), breaks = ntaus, labels = FALSE)]
     xnam <- c("tau", paste0("x", seq_len(s_k)))
@@ -153,45 +157,51 @@ qs_gam <- function(x, y, maxit = 10, ntaus = 10,
     dat <- data.frame(ystar, newx)
     colnames(dat) <- c("y", xnam)
 
-    #initialization
+    # initialization
     fit <- mgcv::gam(y ~ te(tau, x1, k = dims, m = ords),
-            data = dat, sp = c(sp), ...)
+        data = dat, sp = c(sp), ...
+    )
     v_coef <- coef(fit)
 
-    #get info
+    # get info
     v_pred <- predict(fit, dat)
     v_resid <- ystar - v_pred
 
-    #Schlossmacher's method
+    # Schlossmacher's method
     for (iter in seq(maxit)) {
-        #weights
+        # weights
         v_idx <- ystar <= v_pred
         v_weight <- v_wog
         v_weight[v_idx] <- 1 - v_weight[v_idx]
-        v_weight <- v_weight / sqrt(v_resid^2 + 1e-8*max(abs(v_resid))^2)
+        v_weight <- v_weight / sqrt(v_resid^2 + 1e-8 * max(abs(v_resid))^2)
         v_weight <- v_weight / mean(v_weight)
-        #fit the model
+        # fit the model
         fit <- mgcv::gam(y ~ te(tau, x1, k = dims, m = ords),
             data = dat, sp = c(sp), weights = c(v_weight),
-            start = v_coef, ...)
+            start = v_coef, ...
+        )
 
-        #terminate
+        # terminate
         s_diff <- norm_2(v_coef - coef(fit)) / norm_2(v_coef)
         v_coef <- coef(fit)
-        #plot the trace
+        # plot the trace
         if (trace) {
-            fit$x <- x; fit$y <- y; fit$xnam <- xnam
+            fit$x <- x
+            fit$y <- y
+            fit$xnam <- xnam
             sub <- paste("Iter:", iter, "diff:", round(s_diff, 3))
             main <- paste("Smoothing:", fit$sp, "dim:", dims)
             cq_regplot(fit, sub = sub, main = main)
         }
         if (s_diff <= tol) break
-        #get info
+        # get info
         v_pred <- fit$fitted.values
         v_resid <- ystar - v_pred
     }
 
-    fit$x <- x; fit$y <- y; fit$xnam <- xnam
+    fit$x <- x
+    fit$y <- y
+    fit$xnam <- xnam
     if (analysis) {
         dev.new()
         par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))
@@ -199,7 +209,8 @@ qs_gam <- function(x, y, maxit = 10, ntaus = 10,
         plot(fit, pers = TRUE, theta = 30, phi = 40)
         plot(ystar, fit$fitted.values,
             xlab = "Real Data",
-            ylab = "Fitted data", pch = ".", cex = 3)
+            ylab = "Fitted data", pch = ".", cex = 3
+        )
         cq_regplot(fit, sub = sub, main = main)
     }
     fit$ntaus <- ntaus
@@ -233,10 +244,9 @@ qs_gam <- function(x, y, maxit = 10, ntaus = 10,
 #' @return a scam object
 #'
 es_scam <- function(x, y, maxit = 10, ntaus = 10,
-                arg_bs = c("tesmi1", "ps"),
-                opt = "nlm.fd", dims = c(5, 10), ords = NA,
-                analysis=F, trace=F, sp = NULL, tol = 1e-2, ...) {
-
+                    arg_bs = c("tesmi1", "ps"),
+                    opt = "nlm.fd", dims = c(5, 10), ords = NA,
+                    analysis = F, trace = F, sp = NULL, tol = 1e-2, ...) {
     if (is.vector(x)) {
         s_k <- 1
     } else if (is.matrix(x)) {
@@ -245,14 +255,14 @@ es_scam <- function(x, y, maxit = 10, ntaus = 10,
         stop("input x type not support")
     }
 
-    #initialization
+    # initialization
     fit <- cq.reg(x, y, arg_bs, opt, dims, ords, sp = sp, ...)
     v_coef <- coef(fit)
 
-    #repeat Y ntaus times
+    # repeat Y ntaus times
     ystar <- rep(y, ntaus)
 
-    #data
+    # data
     v_taus <- seq(0, 1, length.out = ntaus)
     v_wog <- v_taus[cut(seq_along(ystar), breaks = ntaus, labels = FALSE)]
     xnam <- c("tau", paste0("x", seq_len(s_k)))
@@ -260,37 +270,42 @@ es_scam <- function(x, y, maxit = 10, ntaus = 10,
     dat <- data.frame(ystar, newx)
     colnames(dat) <- c("y", xnam)
 
-    #get info
+    # get info
     v_pred <- predict(fit, dat)
 
-    #Schlossmacher's method
+    # Schlossmacher's method
     for (iter in seq(maxit)) {
-        #weights
+        # weights
         v_idx <- ystar <= v_pred
         v_weight <- v_wog
         v_weight[v_idx] <- 1 - v_weight[v_idx]
         v_weight <- v_weight / mean(v_weight)
-        #fit the model
+        # fit the model
         fit <- scam(y ~ s(tau, x1, k = dims, m = ords, bs = arg_bs),
             data = dat, optimizer = opt, sp = c(sp), weights = c(v_weight),
-            start = NULL, ...)
+            start = v_coef, ...
+        )
 
-        #terminate
+        # terminate
         s_diff <- norm_2(v_coef - coef(fit)) / norm_2(v_coef)
         v_coef <- coef(fit)
-        #plot the trace
+        # plot the trace
         if (trace) {
-            fit$x <- x; fit$y <- y; fit$xnam <- xnam
+            fit$x <- x
+            fit$y <- y
+            fit$xnam <- xnam
             sub <- paste("Iter:", iter, "diff:", round(s_diff, 3))
             main <- paste("Smoothing:", fit$sp, "dim:", dims)
             cq_regplot(fit, sub = sub, main = main)
         }
         if (s_diff <= tol) break
-        #get info
+        # get info
         v_pred <- fit$fitted.values
     }
 
-    fit$x <- x; fit$y <- y; fit$xnam <- xnam
+    fit$x <- x
+    fit$y <- y
+    fit$xnam <- xnam
     if (analysis) {
         dev.new()
         par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))
@@ -298,7 +313,8 @@ es_scam <- function(x, y, maxit = 10, ntaus = 10,
         plot(fit, pers = TRUE, theta = 30, phi = 40)
         plot(ystar, fit$fitted.values,
             xlab = "Real Data",
-            ylab = "Fitted data", pch = ".", cex = 3)
+            ylab = "Fitted data", pch = ".", cex = 3
+        )
         cq_regplot(fit, sub = sub, main = main)
     }
     fit$ntaus <- ntaus
@@ -312,8 +328,8 @@ es_scam <- function(x, y, maxit = 10, ntaus = 10,
 
 #' Expectile Sheets(original)
 #'
-#' Expectile sheet 
-#'  
+#' Expectile sheet
+#'
 #' @param x vector or matrix whose rows contains predictors
 #'          of each observations.
 #' @param y vector of the variable we want to estimate its condtional cdf
@@ -333,9 +349,8 @@ es_scam <- function(x, y, maxit = 10, ntaus = 10,
 #'
 
 es_gam <- function(x, y, maxit = 10, ntaus = 10,
-                dims = c(10, 10), ords = NA,
-                analysis=F, trace=F, sp = NULL, tol = 1e-3, ...) {
-
+                   dims = c(10, 10), ords = NA,
+                   analysis = F, trace = F, sp = NULL, tol = 1e-3, ...) {
     if (is.vector(x)) {
         s_k <- 1
     } else if (is.matrix(x)) {
@@ -343,10 +358,10 @@ es_gam <- function(x, y, maxit = 10, ntaus = 10,
     } else {
         stop("input x type not support")
     }
-    #repeat Y ntaus times
+    # repeat Y ntaus times
     ystar <- rep(y, ntaus)
 
-    #data
+    # data
     v_taus <- seq(0, 1, length.out = ntaus)
     v_wog <- v_taus[cut(seq_along(ystar), breaks = ntaus, labels = FALSE)]
     xnam <- c("tau", paste0("x", seq_len(s_k)))
@@ -354,42 +369,48 @@ es_gam <- function(x, y, maxit = 10, ntaus = 10,
     dat <- data.frame(ystar, newx)
     colnames(dat) <- c("y", xnam)
 
-    #initialization
+    # initialization
     fit <- mgcv::gam(y ~ te(tau, x1, k = dims, m = ords),
-            data = dat, sp = c(sp), ...)
+        data = dat, sp = c(sp), ...
+    )
     v_coef <- coef(fit)
 
-    #get info
+    # get info
     v_pred <- predict(fit, dat)
 
-    #Schlossmacher's method
+    # Schlossmacher's method
     for (iter in seq(maxit)) {
-        #weights
+        # weights
         v_idx <- ystar <= v_pred
         v_weight <- v_wog
         v_weight[v_idx] <- 1 - v_weight[v_idx]
         v_weight <- v_weight / mean(v_weight)
-        #fit the model
+        # fit the model
         fit <- mgcv::gam(y ~ te(tau, x1, k = dims, m = ords),
             data = dat, sp = c(sp), weights = c(v_weight),
-            start = v_coef, ...)
+            start = v_coef, ...
+        )
 
-        #terminate
+        # terminate
         s_diff <- norm_2(v_coef - coef(fit)) / norm_2(v_coef)
         v_coef <- coef(fit)
-        #plot the trace
+        # plot the trace
         if (trace) {
-            fit$x <- x; fit$y <- y; fit$xnam <- xnam
+            fit$x <- x
+            fit$y <- y
+            fit$xnam <- xnam
             sub <- paste("Iter:", iter, "diff:", round(s_diff, 3))
             main <- paste("Smoothing:", fit$sp, "dim:", dims)
             cq_regplot(fit, sub = sub, main = main)
         }
         if (s_diff <= tol) break
-        #get info
+        # get info
         v_pred <- fit$fitted.values
     }
 
-    fit$x <- x; fit$y <- y; fit$xnam <- xnam
+    fit$x <- x
+    fit$y <- y
+    fit$xnam <- xnam
     if (analysis) {
         dev.new()
         par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))
@@ -397,7 +418,8 @@ es_gam <- function(x, y, maxit = 10, ntaus = 10,
         plot(fit, pers = TRUE, theta = 30, phi = 40)
         plot(ystar, fit$fitted.values,
             xlab = "Real Data",
-            ylab = "Fitted data", pch = ".", cex = 3)
+            ylab = "Fitted data", pch = ".", cex = 3
+        )
         cq_regplot(fit, sub = sub, main = main)
     }
     fit$ntaus <- ntaus
